150885cc0f8eee9e7fffb32007b20ccda5de65e8
Sancara <santiagocaraballo@gmail.com>
2022-05-22 11:44:02 -0300

    create CreateView and connect to model

--- classroom/urls.py
+++ classroom/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
-from .views import ContactFormView, HomeView, ThankYou
+from .views import ContactFormView, HomeView, ThankYou, TeacherCreateView
 
 app_name = 'classroom'
 
 urlpatterns = [
     path('', HomeView.as_view() , name='home'),
     path('thanks/', ThankYou.as_view(), name='thanks'),
-    path('contact/', ContactFormView.as_view(), name='contact')
+    path('contact/', ContactFormView.as_view(), name='contact'),
+    path('teacher/', TeacherCreateView.as_view(), name='teacher')
 ]
